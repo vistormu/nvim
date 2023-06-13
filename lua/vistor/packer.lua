8467b6f3bdfc793c288b21f0ade60145ad59f7ed
vistormu <vistormu@gmail.com>
2023-06-13 16:14:48 +0000

    deleted ripgrep from packer

--- lua/vistor/packer.lua
+++ lua/vistor/packer.lua
@@ -5,7 +5,6 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     -- Telescope is a fuzzy finder.
-    use 'BurntSushi/ripgrep'
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         -- or                            , branch = '0.1.x',
