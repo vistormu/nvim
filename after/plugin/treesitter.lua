ebfd0b42acd97e2cea8b80e67d1369bced96e36c
vistormu <vistormu@gmail.com>
2023-07-28 16:35:12 +0000

    blabla

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,13 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "python"},
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.kimchi = {
+    install_info = {
+        url = "~/projects/kimchilang/tree-sitter-kimchi",
+        files = {"src/parser.c"},
+    },
+}
+
+require('nvim-treesitter.configs').setup {
+  ensure_installed = { "python",  "kimchi"},
   
   sync_install = true,
 
@@ -7,7 +15,5 @@ require'nvim-treesitter.configs'.setup {
 
   highlight = {
     enable = true,
-
-    additional_vim_regex_highlighting = true,
   },
 }
